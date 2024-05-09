b83388feb3f047f09757aecfd23854b4850c99af
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-09 18:18:51 +0200

    Added Find.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Find from "./Find";
 
 function App() {
   return (
     <div className="App">
       <h1>Whoa</h1>
       <h2>By Keanu Reeves</h2>
+      <Find />
     </div>
   );
 }
