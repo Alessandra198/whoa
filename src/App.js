16d89c777cf09f6d85d75005ad9dd916d61822d5
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-09 18:16:29 +0200

    Added title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Whoa</h1>
+      <h2>By Keanu Reeves</h2>
+    </div>
+  );
 }
 
 export default App;
