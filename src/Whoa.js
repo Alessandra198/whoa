0eb2b78c994c7d321be49a4b216dc7037d75af1c
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-11 19:32:47 +0200

    Added loop for array

--- src/Whoa.js
+++ src/Whoa.js
@@ -2,7 +2,13 @@ import React from "react";
 
 export default function Whoa(props) {
   if (props.data) {
-    return <div>Hello</div>;
+    return (
+      <div className="Whoa">
+        {props.data.map(function (element, index) {
+          return <div key={index}>{element.full_line}</div>;
+        })}
+      </div>
+    );
   } else {
     return null;
   }
