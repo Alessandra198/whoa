91bbc7af15b93c4e5db3d63fbb5d0f5fef6db123
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-11 19:46:39 +0200

    Fixed video and added bootstrap grids

--- src/Whoa.js
+++ src/Whoa.js
@@ -6,26 +6,32 @@ export default function Whoa(props) {
       <div className="Whoa">
         {props.data.map(function (element, index) {
           return (
-            <div key={index}>
-              <h5>Movie: {element.movie}</h5>
-              <h6>Director: {element.director}</h6>
-              <p>
-                Character: {element.character}
-                <br />
-                Line: {element.full_line}
-                <br />
-                Time: {element.timestamp}
-                <br />
-                Total Whoa in this movie: {element.total_whoas_in_movie}
-              </p>
-              <div>
+            <div key={index} className="row">
+              <div className="col-md-6">
+                <h5>Movie: {element.movie}</h5>
+                <h6>Director: {element.director}</h6>
+                <p>
+                  Character: {element.character}
+                  <br />
+                  Line: {element.full_line}
+                  <br />
+                  Time: {element.timestamp}
+                  <br />
+                  Total Whoa in this movie: {element.total_whoas_in_movie}
+                </p>
+              </div>
+              <div className="col-md-6">
                 <audio controls src={element.audio}>
                   {" "}
                   controlsAudio
                 </audio>
-              </div>
-              <div>
-                <video src={element.video} controls></video>
+                <video
+                  src={element.video["360p"]}
+                  controls
+                  className="img-fluid"
+                >
+                  Video
+                </video>
               </div>
             </div>
           );
