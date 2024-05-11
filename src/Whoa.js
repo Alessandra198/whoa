c9935102dfb2e0073dc7b147ac03849983ed690a
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-11 19:40:42 +0200

    Added every element needed inside the API

--- src/Whoa.js
+++ src/Whoa.js
@@ -5,7 +5,30 @@ export default function Whoa(props) {
     return (
       <div className="Whoa">
         {props.data.map(function (element, index) {
-          return <div key={index}>{element.full_line}</div>;
+          return (
+            <div key={index}>
+              <h5>Movie: {element.movie}</h5>
+              <h6>Director: {element.director}</h6>
+              <p>
+                Character: {element.character}
+                <br />
+                Line: {element.full_line}
+                <br />
+                Time: {element.timestamp}
+                <br />
+                Total Whoa in this movie: {element.total_whoas_in_movie}
+              </p>
+              <div>
+                <audio controls src={element.audio}>
+                  {" "}
+                  controlsAudio
+                </audio>
+              </div>
+              <div>
+                <video src={element.video} controls></video>
+              </div>
+            </div>
+          );
         })}
       </div>
     );
