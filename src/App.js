74d71612b061689f02fd7c7269f0e234eb4972b8
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-12 17:30:15 +0200

    Fixed the responsive ability of the website

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Whoa</h1>
-      <h2>By Keanu Reeves</h2>
-      <Find />
-      <footer className="text-center">
-        Coded with 💪 and 🔥 by{" "}
-        <a href="https://www.alessandrarinaldo.com" alt="Alessandra's website">
-          Alessandra Rinaldo
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Whoa</h1>
+        <h2>By Keanu Reeves</h2>
+        <Find />
+        <footer className="text-center">
+          Coded with 💪 and 🔥 by{" "}
+          <a
+            href="https://www.alessandrarinaldo.com"
+            alt="Alessandra's website"
+          >
+            Alessandra Rinaldo
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
